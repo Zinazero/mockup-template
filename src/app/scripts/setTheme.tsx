668041ecfa000d9534e57157337030ee0f47b8ad
Zinazero <jacobhexamer@gmail.com>
2026-01-29 15:56:27 -0500

    feat: add ThemeToggle and setup

---
+++ src/app/scripts/setTheme.tsx
@@ -0,0 +1,20 @@
+export const SetTheme = () => {
+  return (
+    <script
+      // biome-ignore lint: no user input
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function () {
+            const theme = localStorage.getItem('theme');
+            if (
+              theme === 'dark' ||
+              (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)
+            ) {
+              document.documentElement.classList.add('dark');
+            }
+          })();
+        `,
+      }}
+    />
+  );
+};
---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useEffect, useState } from 'react';
+import { cn } from '../utils/cn';
+
+type ThemeToggleProps = {
+  className?: string;
+};
+export const ThemeToggle = ({ className }: ThemeToggleProps) => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+      setIsDark(true);
+    } else {
+      document.documentElement.classList.remove('dark');
+      setIsDark(false);
+    }
+  }, []);
+
+  const toggleDark = () => {
+    const root = document.documentElement;
+    const newIsDark = !isDark;
+
+    root.classList.toggle('dark', newIsDark);
+    localStorage.setItem('theme', newIsDark ? 'dark' : 'light');
+    setIsDark(newIsDark);
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={toggleDark}
+      className={cn(
+        'text-2xl text-gray-500/50 hover:text-brand-primary active:scale-95 transition',
+        className,
+      )}
+    >
+      <FontAwesomeIcon icon={isDark ? faSun : faMoon} />
+    </button>
+  );
+};
