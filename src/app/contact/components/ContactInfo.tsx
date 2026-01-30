bc8af1384ff6c7e71621fb73c6e46d5d08fe33ee
Zinazero <jacobhexamer@gmail.com>
2026-01-30 18:39:43 -0500

    style: add gap to contact info blocks

--- src/app/contact/components/ContactInfo.tsx
+++ src/app/contact/components/ContactInfo.tsx
@@ -2,7 +2,7 @@ import contactData from '@/src/data/contact.json';
 
 export const ContactInfo = () => {
   return (
-    <div className="flex flex-col w-full">
+    <div className="flex flex-col w-full gap-4">
       {contactData.map(({ header, phoneNumber, address, email }) => (
         <div key={address} className="flex flex-col text-xl">
           {header && <span className="text-2xl font-bold text-brand">{header}</span>}
