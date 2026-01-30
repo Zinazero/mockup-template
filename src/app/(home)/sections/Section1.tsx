8f8213a4c3f0b4c562a9201c6dc3d3184e09f99c
Zinazero <jacobhexamer@gmail.com>
2026-01-29 23:09:12 -0500

    chore: split loc into variables

--- src/app/(home)/sections/Section1.tsx
+++ src/app/(home)/sections/Section1.tsx
@@ -12,9 +12,23 @@ type Location = {
 
 export const Section1 = () => {
   const locations: Location[] = [
-    { location: 'GALT', subLocation: 'CAMBRIDGE', imgSrc: '/locations/galt.png', href: '/galt' },
-    { location: 'NIAGARA FALLS', imgSrc: '/locations/niagara.png', href: '/niagara' },
-    { location: 'PRESTON', subLocation: 'CAMBRIDGE', imgSrc: '/locations/preston.png', href: '/preston' },
+    {
+      location: 'GALT',
+      subLocation: 'CAMBRIDGE',
+      imgSrc: '/locations/galt.png',
+      href: '/galt',
+    },
+    {
+      location: 'NIAGARA FALLS',
+      imgSrc: '/locations/niagara.png',
+      href: '/niagara',
+    },
+    {
+      location: 'PRESTON',
+      subLocation: 'CAMBRIDGE',
+      imgSrc: '/locations/preston.png',
+      href: '/preston',
+    },
   ];
 
   return (
@@ -25,18 +39,18 @@ export const Section1 = () => {
         </h2>
 
         <div className="flex items-center justify-evenly h-120 w-full">
-          {locations.map((loc) => (
+          {locations.map(({ location, subLocation, imgSrc, href }) => (
             <Link
-              href={loc.href}
-              key={loc.location}
+              href={href}
+              key={location}
               className={cn(
                 'relative w-1/4 h-full rounded-t-full overflow-hidden border-brand border-2',
                 'hover:brightness-110 hover:scale-102 active:scale-95 transition group',
               )}
             >
               <Image
-                src={loc.imgSrc}
-                alt={`${loc.location} Storefront`}
+                src={imgSrc}
+                alt={`${location} Storefront`}
                 className="absolute top-0 left-0 w-full h-full object-cover"
                 fill
               />
@@ -44,11 +58,11 @@ export const Section1 = () => {
               <div
                 className={cn(
                   'flex flex-col items-center justify-center absolute left-1/2 -translate-x-1/2 bottom-1/20',
-                  'bg-light h-1/8 w-4/5 p-4 font-bold rounded-xl group-hover:text-brand',
+                  'bg-light h-1/6 w-4/5 p-4 font-bold rounded-xl',
                 )}
               >
-                {loc.subLocation && <span className="text-sm">{loc.subLocation}</span>}
-                <span className="text-xl">{loc.location}</span>
+                {subLocation && <span className="text-sm">{subLocation}</span>}
+                <span className="text-xl">{location}</span>
               </div>
             </Link>
           ))}
