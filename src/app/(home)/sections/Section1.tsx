31869916affccf555c7f021e9883a06d48b6171f
Zinazero <jacobhexamer@gmail.com>
2026-01-29 21:01:50 -0500

    feat: add locations section

--- src/app/(home)/sections/Section1.tsx
+++ src/app/(home)/sections/Section1.tsx
@@ -1,7 +1,54 @@
+import { cn } from '@/src/utils/cn';
+import Image from 'next/image';
+import Link from 'next/link';
+
+type Location = {
+  location: string;
+  subLocation?: string;
+  imgSrc: string;
+  href: string;
+};
+
 export const Section1 = () => {
+  const locations: Location[] = [
+    { location: 'GALT', subLocation: 'CAMBRIDGE', imgSrc: '/locations/galt.png', href: '/galt' },
+    { location: 'NIAGARA FALLS', imgSrc: '/locations/niagara.png', href: '/niagara' },
+    { location: 'PRESTON', subLocation: 'CAMBRIDGE', imgSrc: '/locations/preston.png', href: '/preston' },
+  ];
+
   return (
-    <section>
-      
+    <section className="flex flex-col items-center gap-16 h-200 w-full max-w-350 p-10">
+      <h1 className="font-bold text-6xl dark:text-light">LOCATIONS</h1>
+
+      <div className="flex items-center justify-evenly h-2/3 w-full">
+        {locations.map((loc) => (
+          <Link
+            href={loc.href}
+            key={loc.location}
+            className={cn(
+              'relative w-1/4 h-full rounded-t-full overflow-hidden',
+              'hover:brightness-125 active:scale-95 transition group',
+            )}
+          >
+            <Image
+              src={loc.imgSrc}
+              alt={`${loc.location} Storefront`}
+              className="absolute top-0 left-0 w-full h-full object-cover"
+              fill
+            />
+
+            <div
+              className={cn(
+                'flex flex-col items-center justify-center absolute left-1/2 -translate-x-1/2 bottom-1/20',
+                'bg-light h-1/8 w-4/5 p-4 font-bold rounded-xl group-hover:text-brand',
+              )}
+            >
+              {loc.subLocation && <span className="text-sm">{loc.subLocation}</span>}
+              <span className="text-xl">{loc.location}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </section>
   );
 };
