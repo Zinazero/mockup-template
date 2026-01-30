be56f50f1f8e3ced64a22361f864849df36f35f3
Zinazero <jacobhexamer@gmail.com>
2026-01-29 21:38:58 -0500

    fix: make thge stroke appear

--- src/app/(home)/sections/Section1.tsx
+++ src/app/(home)/sections/Section1.tsx
@@ -1,7 +1,11 @@
+import { UnderlineHeader } from "@/src/components/UnderlineHeader";
+
 export const Section1 = () => {
   return (
-    <>
-      <></>
-    </>
+    <section className="p-10">
+      <h1>
+        <UnderlineHeader text="SECTION 1" level={1} />
+      </h1>
+    </section>
   );
 };
